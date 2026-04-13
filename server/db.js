990aee031fd80376b94f3e0e631a48713fe8094f
Erwan Marega <maregaerwan@gmail.com>
2026-04-13 16:37:48 +0200

    fix : change mount path

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'spotcalendar.db'));
+const db = new Database(path.join(process.env.DATA_DIR || __dirname, 'spotcalendar.db'));
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS users (
